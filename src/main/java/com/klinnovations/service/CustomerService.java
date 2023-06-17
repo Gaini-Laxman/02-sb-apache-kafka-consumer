3616c417e20388a078b24cd06c6e6c556657eefb
Gaini Laxman <gainilaxman12@gmail.com>
2023-06-17 10:54:07 +0530

    initial commit

---
+++ src/main/java/com/klinnovations/service/CustomerService.java
@@ -0,0 +1,31 @@
+package com.klinnovations.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.klinnovations.model.Customer;
+import com.klinnovations.util.KafkaConstants;
+
+/**
+ * This class is used to perform business operation
+ * 
+ * @author Ashok
+ *
+ */
+
+@Service("customerService")
+public class CustomerService {
+
+	/**
+	 * This method is used to consumer messages from kafka topic
+	 * 
+	 * @param c
+	 * @return
+	 */
+	@KafkaListener(topics = KafkaConstants.TOPIC, groupId = KafkaConstants.GROUP_ID)
+	public Customer listener(Customer c) {
+		System.out.println("***Msg recieved from Kafka Topic ::" + c);
+		return c;
+	}
+
+}
